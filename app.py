72dbbb1eb9bc2aec8c157e886e9f9e85e860cd85
mohammadMichael <101001742+mohammadMichael@users.noreply.github.com>
2026-06-26 20:15:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import time
+from telegram_bot import send_message
+from market import get_market_data
+from filters import apply_filter
 
-print("APP STARTED", flush=True)
-
-try:
-    from market import get_market_data
-    print("MARKET IMPORT OK", flush=True)
-except Exception as e:
-    print("MARKET IMPORT FAILED:", e, flush=True)
+print("BOT STARTED", flush=True)
+send_message("📡 Bot Started")
 
 while True:
-    print("loop alive", flush=True)
-    time.sleep(10)
+    print("GET DATA", flush=True)
+
+    data = get_market_data()
+    print("DATA LEN:", len(data), flush=True)
+
+    for item in data:
+        result = apply_filter(item)
+
+        print("RESULT:", result, flush=True)
+
+        if result:
+            send_message(f"{result['symbol']} → {result['score']}")
+
+    time.sleep(30)
