bf87539698b3a0d165f25378c6346c015de20366
mohammadMichael <101001742+mohammadMichael@users.noreply.github.com>
2026-06-26 20:21:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,18 @@ send_message("📡 Bot Started")
 while True:
     data = get_market_data()
 
+    print("DATA LEN:", len(data), flush=True)
+
+    found = 0
+
     for item in data:
         result = apply_filter(item)
 
         if result:
+            found += 1
+            print("SIGNAL:", result, flush=True)
             send_message(f"{result['symbol']} → {result['score']}")
+
+    print("FOUND SIGNALS:", found, flush=True)
 
     time.sleep(60)
