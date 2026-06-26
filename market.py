6db6c7cc378367e5a25662be7eea3386c2446c03
mohammadMichael <101001742+mohammadMichael@users.noreply.github.com>
2026-06-26 19:29:37 +0000

    Update market.py

--- market.py
+++ market.py
@@ -1,25 +1,40 @@
 import requests
 
 def get_market_data():
-    url = "https://cdn.tsetmc.com/api/ClosingPrice/GetMarketWatch?market=0"
+    url = "https://old.tsetmc.com/tsev2/data/MarketWatchPlus.aspx"
 
-    response = requests.get(url)
-    data = response.json()
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(url, headers=headers)
+    text = r.text
 
     results = []
 
-    for item in data.get("marketWatch", []):
+    # دیتا به صورت ; جداست
+    rows = text.split(";")
+
+    for row in rows:
+        parts = row.split("@")
+
         try:
+            if len(parts) < 10:
+                continue
+
+            symbol = parts[0]
+
             results.append({
-                "symbol": item.get("lVal18AFC"),   # نام سهم
+                "symbol": symbol,
                 "cs": 1,
-                "tvol": item.get("qTotTran5J"),    # حجم
-                "pl": item.get("pClosing"),        # قیمت پایانی
-                "tmax": item.get("priceMax"),      # سقف
-                "tmin": item.get("priceMin"),      # کف
-                "qd1": item.get("zTotTran"),       # خریدار
-                "qo1": item.get("count"),          # فروشنده
+                "tvol": float(parts[3] or 0),
+                "pl": float(parts[2] or 0),
+                "tmax": float(parts[6] or 0),
+                "tmin": float(parts[7] or 0),
+                "qd1": float(parts[8] or 0),
+                "qo1": float(parts[9] or 0),
             })
+
         except:
             continue
 
