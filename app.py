e42a303a2cd56e860470974c4f8451073329d675
mohammadMichael <101001742+mohammadMichael@users.noreply.github.com>
2026-06-26 20:14:10 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import time
 
-print("Bot Started", flush=True)
+print("APP STARTED", flush=True)
+
+try:
+    from market import get_market_data
+    print("MARKET IMPORT OK", flush=True)
+except Exception as e:
+    print("MARKET IMPORT FAILED:", e, flush=True)
 
 while True:
     print("loop alive", flush=True)
