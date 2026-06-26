69c4d85c950d65296a8a672f0911e5dacaf0ca23
mohammadMichael <101001742+mohammadMichael@users.noreply.github.com>
2026-06-26 19:23:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import time
+from telegram_bot import send_message
 
 print("Bourse Bot Started", flush=True)
 
+send_message("📡 Bot Started Successfully")
+
 while True:
     print("alive...", flush=True)
-    time.sleep(5)
+    time.sleep(10)
